04db48ccebf574ba6e8ab72327f35ad093e9b0c9
mixed_nuts_nuts <french0719@gmail.com>
2022-07-19 16:54:56 +0700

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const count = 0;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  useEffect(() => {
+    console.log("Hello Hooks");
+  }, [count]);
   return (
     <div className="App">
       <h1>UseState</h1>
       <p>{count}</p>
+      <button onClick={handleClick}>+</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createContext, useContext } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+type Infomation = {
+  name: string;
+  age: number;
+};
+
+const myInfo: Infomation = {
+  name: "K659903",
+  age: 24,
+};
+
+const MyInfoContext = createContext(myInfo);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MyInfoContext.Provider value={myInfo}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MyInfoContext.Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
